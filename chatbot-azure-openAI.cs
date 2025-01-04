d89626fe29d17d67a6487a0533d32f1144f6216f
Daniel Maldonado <110861879+danieldlcm86@users.noreply.github.com>
2025-01-03 22:43:50 -0600

    Simple Chatbot build with C# and Azure OpenAI

---
+++ chatbot-azure-openAI.cs
@@ -0,0 +1,40 @@
+﻿using Azure;
+using OpenAI.Chat;
+using Microsoft.Data.SqlClient;
+using Azure.AI.OpenAI;
+
+string key = "AZURE_OPENAI_ENDPOINT";
+string endpoint = "AZURE_OPENAI_KEY";
+string model = "AZURE_OPENAI_MODEL_NAME";
+
+// Add the client info
+// Initialize the Azure OpenAI client with key-based authentication  
+AzureOpenAIClient azureClient = new(
+    new Uri(endpoint),
+    new AzureKeyCredential(key));
+
+// Assign the model to the client
+ChatClient chatClient = azureClient.GetChatClient(model);
+
+// Prepare the request for chat generation
+ChatCompletionOptions completionOptions = new()
+{
+    Temperature = (float)0.7,
+};
+
+// SystemChatMessage and userChatMessage
+string systemChatMsg = "YOUR_SYSTEM_MESSAGE";
+string userChatMsg = "Necesito un modelo entidad relación";
+
+// Chat List Messages
+List<ChatMessage> messages = [
+    new SystemChatMessage(systemChatMsg),
+    new UserChatMessage(userChatMsg),
+
+    ];
+
+ChatCompletion completion = chatClient.CompleteChat(messages, completionOptions);
+
+// Print the response 
+Console.WriteLine($"{completion.Role}: {completion.Content[0].Text}");
+
